a6ef4c91a0ce99cb98515125c01563a3d922d773
kibaara-wangu <kibaarawangu@gmail.com>
2024-03-25 08:25:28 +0300

    added files

---
+++ src/Main.kt
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var sentence = ("Barnie bakes brown bagels and buns")
+    println(sentence)
+
+
+    var numbers = 23+10+45
+    println(numbers)
+
+    var count = numbers.count()
+    println(numbers)
+
+    var average = numbers/3
+    println(average)
+
+
+
+}
+fun sentence(name:String):String{
+    return name
+}
+
+
+
+
+
+fun add(sum:Int,count:Int,average:Int){
+    var addition = arrayOf<Int>();
+    println(sum)
+    println(count)
+}
+
+
+
+
+
+
